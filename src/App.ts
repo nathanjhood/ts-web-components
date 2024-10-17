5eb56087891de23a58a722a72e7e79d5faf658db
StoneyDSP <nathanjhood@googlemail.com>
2024-10-17 02:45:44 +0200

    improved clarity of AppComponent structure
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- src/App.ts
+++ src/App.ts
@@ -3,10 +3,11 @@ import logo = require('./logo.svg');
 
 const App = (): HTMLElement => {
   //
+  console.log('constructing App()');
+  //
   const logoSrc = 'data:image/svg+xml;base64,' + logo;
   //
-  if (!window.customElements.get('app-component')) {
-    console.log('constructing App()');
+  if (window.customElements.get('app-component') === undefined) {
     window.customElements.define(
       'app-component',
       /**
@@ -15,21 +16,27 @@ const App = (): HTMLElement => {
        */
       class AppComponent extends HTMLElement {
         /**
-         *
+         * Constructs a new instance of this component.
          */
         constructor() {
           super();
           console.debug('constructor()');
         }
         /**
+         * Performs the required setup steps for this component instance.
          *
+         * @returns {void}
+         * @private
          */
         private setup(): void {
           this.innerHTML = this.render();
           console.debug('setup()');
         }
         /**
+         * Fires whenever this component instance is connected to a page.
+         * Inherited from {@link HTMLElement}.
          *
+         * @returns {void}
          */
         connectedCallback(): void {
           this.setup();
@@ -37,26 +44,44 @@ const App = (): HTMLElement => {
             '<app-component connectedCallback()> - element added to page.'
           );
         }
-
+        /**
+         * Fires whenever this component instance is disconnected from a page.
+         * Inherited from {@link HTMLElement}.
+         *
+         * @returns {void}
+         */
         disconnectedCallback(): void {
           console.debug(
             '<app-component disconnectedCallback()> - element removed from page.'
           );
         }
-
+        /**
+         * Fires whenever this component instance in moved to a new page.
+         * Inherited from {@link HTMLElement}.
+         *
+         * @returns {void}
+         */
         adoptedCallback(): void {
           console.debug(
             '<app-component adoptedCallback()> - element moved to new page.'
           );
         }
-
+        /**
+         * Fires whenever this components' attributes have changed.
+         * Inherited from {@link HTMLElement}.
+         *
+         * @param {string} name
+         * @param {string} oldValue
+         * @param {string} newValue
+         * @returns {void}
+         */
         attributeChangedCallback(
           name: string,
           oldValue: string,
           newValue: string
         ): void {
           console.debug(
-            '<app-component attributeChangedCallback()> attributes changed.',
+            '<app-component attributeChangedCallback()> attributes changed:',
             {
               name: name,
               oldValue: oldValue,
@@ -64,6 +89,10 @@ const App = (): HTMLElement => {
             }
           );
         }
+        /**
+         * Renders this component instance to a string.
+         * @returns {string}
+         */
         private render(): string {
           return `
 <div class="App">

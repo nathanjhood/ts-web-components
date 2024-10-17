3d31387e1353e5fb6cc50b28669144ca7e78c297
StoneyDSP <nathanjhood@googlemail.com>
2024-10-17 02:56:25 +0200

    cleanup render function
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- src/index.ts
+++ src/index.ts
@@ -9,49 +9,48 @@ import App = require('./App');
  * Renders error and warning messages in case of failure.
  */
 const render = <T extends HTMLElement = HTMLElement>(element: () => T) => {
+  // prepare to collect any errors and warnings
   const warnings: Error[] = [];
   const errors: Error[] = [];
 
+  // get a reference to the HTML element with `id="root"`
   const root: HTMLElement | null = document.getElementById('root');
 
   if (root === null) {
     errors.push(new Error('Missing HTML tag with id="root"'));
   } else {
-    if (element) {
-      const el: T = element();
-
-      const shadowRoot = root.attachShadow({ mode: 'open' });
-      const extStylesheet: HTMLLinkElement =
-        document.createElement<'link'>('link');
-      extStylesheet.setAttribute('rel', 'stylesheet');
-      extStylesheet.setAttribute(
-        'href',
-        `${env['PUBLIC_URL']}static/css/index.css`
-      );
-      // Attach the created elements to the shadow dom
-      shadowRoot.appendChild<HTMLLinkElement>(extStylesheet);
-      shadowRoot.appendChild<ReturnType<typeof element>>(el);
-    }
+    // create a shadow root and attach it to the 'root' element...
+    const shadowRoot: ShadowRoot = root.attachShadow({ mode: 'open' });
+    // for CSS stylesheets to work, create a link pointing at the CSS...
+    const extStylesheet: HTMLLinkElement =
+      document.createElement<'link'>('link');
+    extStylesheet.setAttribute('rel', 'stylesheet');
+    extStylesheet.setAttribute(
+      'href',
+      `${env['PUBLIC_URL']}static/css/index.css`
+    );
+    // Attach the created elements to the shadow dom
+    shadowRoot.appendChild<HTMLLinkElement>(extStylesheet);
+    shadowRoot.appendChild<T>(element());
   }
 
   // error reporting
   if (errors.length > 0 || warnings.length > 0) {
-    //
+    // put all errors and warnings (if any) into one array
     const messages: Error[] = errors.concat(warnings);
     //
-    messages.forEach(async (message) => {
-      //
+    messages.forEach((message) => {
+      // attach each message to the document
       const pre = document.createElement('pre');
       const code = document.createElement('code');
       code.innerText += message.stack + '\n';
-      //
       pre.appendChild(code);
       document.body.appendChild(pre);
-      //
+      // also log the message to the console
       console.error(message.message);
       return message;
     });
   }
 };
 
-export = render<ReturnType<typeof App>>(App);
+export = render<HTMLElement>(App);
